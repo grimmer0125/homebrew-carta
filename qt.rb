a357fe7ed9fd79c8cd0dfbab2741357453f6b113
Grimmer <grimmer0125@gmail.com>
2017-05-22 11:41:13 +0800

    Try to customize qt5.8

--- qt.rb
+++ qt.rb
@@ -1,43 +1,53 @@
-# Patches for Qt5 must be at the very least submitted to Qt's Gerrit codereview
+
+# Patches for Qt must be at the very least submitted to Qt's Gerrit codereview
 # rather than their bug-report Jira. The latter is rarely reviewed by Qt.
 class Qt < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/5.7/5.7.1/single/qt-everywhere-opensource-src-5.7.1.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/download.qt-project.org/official_releases/qt/5.7/5.7.1/single/qt-everywhere-opensource-src-5.7.1.tar.xz"
-  sha256 "46ebca977deb629c5e69c2545bc5fe13f7e40012e5e2e451695c583bd33502fa"
-  head "https://code.qt.io/qt/qt5.git", :branch => "5.7", :shallow => false
+  revision 2
+  head "https://code.qt.io/qt/qt5.git", :branch => "5.8", :shallow => false
+
+  stable do
+    url "https://download.qt.io/official_releases/qt/5.8/5.8.0/single/qt-everywhere-opensource-src-5.8.0.tar.xz"
+    mirror "https://www.mirrorservice.org/sites/download.qt-project.org/official_releases/qt/5.8/5.8.0/single/qt-everywhere-opensource-src-5.8.0.tar.xz"
+    sha256 "0f4c54386d3dbac0606a936a7145cebb7b94b0ca2d29bc001ea49642984824b6"
+
+    # Upstream issue "Qt5.8: macOS, designer examples fails to compile"
+    # Reported 15 Dec 2016 https://bugreports.qt.io/browse/QTBUG-57656
+    # Upstream PR from 31 Jan 2017 "fix installation of header-only frameworks"
+    # See https://codereview.qt-project.org/#/c/184053/1
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/634a19fb/qt5/QTBUG-57656.patch"
+      sha256 "a69fc727f4378dbe0cf05ecf6e633769fe7ee6ea52b1630135a05d5adfa23d87"
+    end
+  end
 
 #   bottle do
-#     sha256 "e8eb4c729e6c3a788c982a740efc95bc96c0055ae929c5662b603bbd7a5a76b0" => :sierra
-#     sha256 "c1a3f9d43de1cf014060b6d66575ae97e6ae77d9102c28971884a7b28a654e0e" => :el_capitan
-#     sha256 "a5fe56be34bf03a68d0c42aae04d381ce1254cd3e72959793af33f4800f602ec" => :yosemite
+#     sha256 "eff56a84940888c0e9bcb6a95cd5b4d434bee36c6df3162d954cb15f10627c32" => :sierra
+#     sha256 "0f322056717bb8f821aa809c79a3842ce914ce112bc273bbee9f2f05f0819fe3" => :el_capitan
+#     sha256 "762149a177db074d9ee721395739bcc7c264a6fac0604a6a1bc2f913098df2bc" => :yosemite
 #   end
 
-  keg_only :versioned_formula
+  keg_only "Qt 5 has CMake issues when linked"
 
   option "with-docs", "Build documentation"
   option "with-examples", "Build examples"
   option "with-qtwebkit", "Build with QtWebkit module"
-
-  deprecated_option "qtdbus" => "with-dbus"
-  deprecated_option "with-d-bus" => "with-dbus"
 
   # OS X 10.7 Lion is still supported in Qt 5.5, but is no longer a reference
   # configuration and thus untested in practice. Builds on OS X 10.7 have been
   # reported to fail: <https://github.com/Homebrew/homebrew/issues/45284>.
   depends_on :macos => :mountain_lion
 
-  depends_on "dbus" => :optional
-  depends_on :mysql => :optional
   depends_on "pkg-config" => :build
-  depends_on :postgresql => :optional
   depends_on :xcode => :build
+  depends_on :mysql => :optional
+  depends_on :postgresql => :optional
 
   # http://lists.qt-project.org/pipermail/development/2016-March/025358.html
   resource "qt-webkit" do
-    url "https://download.qt.io/community_releases/5.7/5.7.1/qtwebkit-opensource-src-5.7.1.tar.xz"
-    sha256 "a46cf7c89339645f94a5777e8ae5baccf75c5fc87ab52c9dafc25da3327b5f03"
+    url "https://download.qt.io/community_releases/5.8/5.8.0-final/qtwebkit-opensource-src-5.8.0.tar.xz"
+    sha256 "79ae8660086bf92ffb0008b17566270e6477c8fa0daf9bb3ac29404fb5911bec"
   end
 
   # Restore `.pc` files for framework-based build of Qt 5 on OS X. This
@@ -45,9 +55,9 @@ class Qt < Formula
   # between the 5.5.1 and 5.6.0 releases. (Remove this as soon as feasible!)
   #
   # Core formulae known to fail without this patch (as of 2016-10-15):
-  #   * gnuplot  (with `--with-qt5` option)
-  #   * mkvtoolnix (with `--with-qt5` option, silent build failure)
-  #   * poppler    (with `--with-qt5` option)
+  #   * gnuplot  (with `--with-qt` option)
+  #   * mkvtoolnix (with `--with-qt` option, silent build failure)
+  #   * poppler    (with `--with-qt` option)
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/e8fe6567/qt5/restore-pc-files.patch"
     sha256 "48ff18be2f4050de7288bddbae7f47e949512ac4bcd126c2f504be2ac701158b"
@@ -65,34 +75,47 @@ class Qt < Formula
       -qt-freetype
       -qt-pcre
       -nomake tests
-      -no-rpath
       -pkg-config
+      -dbus-runtime
+      -skip qtwebengine
+      -skip qtserialbus 
+      -skip qtserialport 
+      -skip qtsensors 
+      -skip qtscript 
+      -skip qtlocation 
+      -skip qtconnectivity 
+      -skip qtandroidextras 
+      -skip qtquickcontrols 
+      -skip qtquickcontrols2
+      -no-mips_dsp 
+      -no-mips_dspr2
+      -no-pps 
+      -no-slog2 
+      -no-imf 
+      -no-lgmon 
     ]
 
     args << "-nomake" << "examples" if build.without? "examples"
 
     if build.with? "mysql"
       args << "-plugin-sql-mysql"
-      inreplace "qtbase/configure", /(QT_LFLAGS_MYSQL_R|QT_LFLAGS_MYSQL)=\`(.*)\`/, "\\1=\`\\2 | sed \"s/-lssl -lcrypto//\"\`"
+      (buildpath/"brew_shim/mysql_config").write <<-EOS.undent
+        #!/bin/sh
+        if [ x"$1" = x"--libs" ]; then
+          mysql_config --libs | sed "s/-lssl -lcrypto//"
+        else
+          exec mysql_config "$@"
+        fi
+      EOS
+      chmod 0755, "brew_shim/mysql_config"
+      args << "-mysql_config" << buildpath/"brew_shim/mysql_config"
     end
 
     args << "-plugin-sql-psql" if build.with? "postgresql"
 
-    if build.with? "dbus"
-      dbus_opt = Formula["dbus"].opt_prefix
-      args << "-I#{dbus_opt}/lib/dbus-1.0/include"
-      args << "-I#{dbus_opt}/include/dbus-1.0"
-      args << "-L#{dbus_opt}/lib"
-      args << "-ldbus-1"
-      args << "-dbus-linked"
-    else
-      args << "-no-dbus"
-    end
-
-    if build.with? "qtwebkit"
-      (buildpath/"qtwebkit").install resource("qt-webkit")
-      inreplace ".gitmodules", /.*status = obsolete\n((\s*)project = WebKit\.pro)/, "\\1\n\\2initrepo = true"
-    end
+    (buildpath/"qtwebkit").install resource("qt-webkit")
+    inreplace ".gitmodules", /.*status = obsolete\n((\s*)project = WebKit\.pro)/, "\\1\n\\2initrepo = true"
+    
 
     system "./configure", *args
     system "make"
@@ -114,21 +137,12 @@ class Qt < Formula
       include.install_symlink path => path.parent.basename(".framework")
     end
 
-    # configure saved PKG_CONFIG_LIBDIR set up by superenv; remove it
-    # see: https://github.com/Homebrew/homebrew/issues/27184
-    inreplace prefix/"mkspecs/qconfig.pri",
-              /\n# pkgconfig\n(PKG_CONFIG_(SYSROOT_DIR|LIBDIR) = .*\n){2}\n/,
-              "\n"
-
     # Move `*.app` bundles into `libexec` to expose them to `brew linkapps` and
-    # because we don't like having them in `bin`. Also add a `-qt5` suffix to
-    # avoid conflict with the `*.app` bundles provided by the `qt` formula.
-    # (Note: This move/rename breaks invocation of Assistant via the Help menu
+    # because we don't like having them in `bin`.
+    # (Note: This move breaks invocation of Assistant via the Help menu
     # of both Designer and Linguist as that relies on Assistant being in `bin`.)
     libexec.mkpath
-    Pathname.glob("#{bin}/*.app") do |app|
-      mv app, libexec/"#{app.basename(".app")}-qt5.app"
-    end
+    Pathname.glob("#{bin}/*.app") { |app| mv app, libexec }
   end
 
   def caveats; <<-EOS.undent
